2bf247c20daeb408a1aae23124149cc779082444
Arman Ebrahimi <armanebrhm@gmail.com>
2023-06-07 17:02:27 +0200

    adding controller.

---
+++ src/main/java/com/example/spring5jokesappv2/Model/Joke.java
@@ -0,0 +1,13 @@
+package com.example.spring5jokesappv2.Model;
+
+public class Joke {
+    private String quote;
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
---
+++ src/main/java/com/example/spring5jokesappv2/service/JokeService.java
@@ -0,0 +1,7 @@
+package com.example.spring5jokesappv2.service;
+
+import org.springframework.stereotype.Service;
+
+public interface JokeService {
+    String getJoke();
+}
